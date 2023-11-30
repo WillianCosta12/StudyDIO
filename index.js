446d3bf47605398d3847145c2fba9cd8416f7714
Willian Costa <68620087+WillianCosta12@users.noreply.github.com>
2023-11-30 13:50:31 -0300

    addRanked

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 let herois = [{
         nome: "Kartz",
         xp: 500,
-        nivel: ""
+        nivel: "",
+        vitorias: 30,
+        derrotas: 2,
     },
     {
         nome: "Michael",
         xp: 5000,
-        nivel: ""
+        nivel: "",
+        vitorias: 90,
+        derrotas: 10,
     },
     {
         nome: "Xec",
         xp: 67050,
-        nivel: ""
+        nivel: "",
+        vitorias: 10,
+        derrotas: 0,
     },
     {
         nome: "WLSK",
         xp: 2300,
-        nivel: ""
+        nivel: "",
+        vitorias: 20,
+        derrotas: 45,
     }
 ]
 
@@ -42,7 +50,34 @@ herois.forEach(heroi => {
         
 });
 
+function calculoRanked(heroi){
+    let ranked = ""
+
+    if (heroi.vitorias <= 10) {
+        ranked = "Ferro"
+    } else if (heroi.vitorias > 10 && heroi.vitorias <= 20) {
+        ranked = "Bronze"
+    } else if (heroi.vitorias > 20 && heroi.vitorias <= 50) {
+        ranked = "Prata"
+    } else if (heroi.vitorias > 50 && heroi.vitorias <= 80) {
+        ranked = "Ouro"
+    } else if (heroi.vitorias > 80 && heroi.vitorias <= 90) {
+        ranked = "Diamante"
+    } else if (heroi.vitorias > 90 && heroi.vitorias <= 100) {
+        ranked = "Lendário"
+    } else if (heroi.vitorias > 100) {
+        ranked = "Imortal"
+    }
+
+    return ranked
+
+}
+
 herois.forEach(heroi => {
     console.log(`O Herói de nome ${heroi.nome} está no nível de ${heroi.nivel}`)
-});
 
+    let retorno = calculoRanked(heroi);
+
+    console.log(`O Herói tem de saldo de ${heroi.vitorias - heroi.derrotas} está no nível de ${retorno}`)
+
+});
